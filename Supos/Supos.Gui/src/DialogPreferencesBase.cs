bdbd4a107842803f4a067f39a106132d65d28d02
xavier.thomas.1980 <xavier.thomas.1980@c15b6ff3-3b36-0410-a9da-bbba2b57a84c>
2009-02-03 16:35:16 +0000

    Basic preferences dialog

---
+++ Supos/Supos.Gui/src/DialogPreferencesBase.cs
@@ -0,0 +1,42 @@
+// DialogPreferencesBase.cs created with MonoDevelop
+// User: xavier at 01:25 3/02/2009
+//
+// To change standard headers go to Edit->Preferences->Coding->Standard Headers
+//
+
+using System;
+using Gtk;
+
+namespace Supos.Gui
+{
+	
+	
+	public class DialogPreferencesBase : Gtk.Dialog
+	{
+		private Notebook notebook;
+		private FormDatabasePreferences dbForm;
+		
+		public DialogPreferencesBase(Window parent) : base("Preferences", parent, DialogFlags.Modal, Stock.Ok, ResponseType.Ok, Stock.Cancel, ResponseType.Cancel)
+		{
+			notebook = new Notebook();
+			
+			dbForm = new FormDatabasePreferences();
+			dbForm.BorderWidth = 6;
+			notebook.AppendPage(dbForm, new Label("Database") );
+			
+			this.VBox.PackStart(notebook, true, true, 6);
+			this.ShowAll();
+		}
+		
+		public void SetDatabaseSettingsFromConfig(string confstring)
+		{
+			dbForm.SetFromConfig(confstring);
+		}
+		
+		public string GetDatabaseConfig()
+		{
+			return dbForm.GetConfig();
+		}
+		
+	}
+}
